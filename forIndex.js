a485eb8eebf6bab1b0f2197c9e5e67e02a1ae9c0
Riju Sikdar <rijusikdar@gmail.com>
2018-02-14 16:07:09 -0500

    mobile scaling successful

--- forIndex.js
+++ forIndex.js
@@ -4,9 +4,63 @@ function redirect(name){
 }
 
 $(document).ready(function(){
+		
+	if (!localStorage.getItem("reload")) {
+
+	    localStorage.setItem("reload", "true");
+	    location.reload();
+	    console.log("1")
+	}
+
+	else {
+	    localStorage.removeItem("reload");
+	    console.log("2")
+	}
+
+
+
+	$( window ).resize(function() {
+		$( "div" ).each(function() {
+			if($(this).hasClass('project')){
+	  			$(this).height($(this).children().children().height() + 50 );
+
+			}
+		});
+
+		if($('body').width() < '920'){
+			$(this).css("display", "block");
+			/*$('body').css("min-width", "656px");
+			$('body').css("width", "100%");
+			$('body>.rest').css("padding-left", "1%");
+			$('body>.rest').css("width", "95%");*/
+			$('body > div.rest').css("padding-left", $('body').width()*.03 + "%");
+		}else{
+			$(this).css("display", "inline-block");
+			/*
+			$('body>.rest').css("width", "90%");*/
+			$('body > div.rest').css("padding-left", "3%");
+			$('body > div.rest').css("padding-right", "1%");
+		}
+	});
+
 	$( "div" ).each(function() {
 		if($(this).hasClass('project')){
 	  		$(this).height($(this).children().children().height() + 50 );
 		}
+		if($('body').width() < '920'){
+			$(this).css("display", "block");
+			/*$('body').css("min-width", "656px");
+			$('body').css("width", "100%");
+			$('body>.rest').css("padding-left", "1%");
+			$('body>.rest').css("width", "95%");*/
+			$('body > div.rest').css("padding-left", $('body').width()*.03 + "%");
+		}else{
+			$(this).css("display", "inline-block");
+			/*
+			$('body>.rest').css("width", "90%");*/
+			$('body > div.rest').css("padding-left", "3%");
+			$('body > div.rest').css("padding-right", "1%");
+		}
 	});
+
 });
