1d22e994b076d949039e570d41875674b72b55f6
Riju Sikdar <rijusikdar@gmail.com>
2018-09-23 12:09:43 -0400

    dark theme cookie progress

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+var darkStatus = 0;
+
 $(document).ready(function(){
-	$("h1").fadeIn("slow", function(){}).queue();
+	//alert( $.cookie("darkTheme") );
+	$.cookie("darkTheme", "on");
+
+	var dark = $.cookie("darkTheme");
+	if(dark == ""){
+		writeDarkCookie(0);
+	}else if(dark == "on"){
+		darkOn();
+		darkStatus = 1;
+	}
+
+	console.log(dark);
+
+	$("h1").fadeIn("slow", function(){})
 
 	$("hr").fadeIn("slow", function(){});
 
 	$("h2").fadeIn("slow", function(){});
 	$("h2").fadeIn("slow", function(){});
+
+	$("#dark").fadeIn("slow", function(){});
 
 });
 
@@ -12,4 +29,51 @@ function projects(){
 	console.log("hre");
     $("#projects").hide();
     $("#subProjects").fadeIn( 400, function(){} );
+}
+
+function darkOn(){
+
+	$("body").css("background-color", "#1e1e1e");
+	$("h1").css("color", "#eeeeee");
+	$("h2").css("color", "#eeeeee");
+	$("h3").css("color", "#eeeeee");
+	$("hr").css("color", "#eeeeee");
+
+	writeDarkCookie(0);
+}
+
+function darkOff(){
+
+	$("main").css("background-color", "#eee");
+	$("h1").css("color", "#010101");
+	$("h2").css("color", "#010101");
+	$("h3").css("color", "#010101");
+	$("hr").css("color", "#010101");
+
+	writeDarkCookie(1);
+}
+
+function writeDarkCookie(on) {
+	if(on == 1){
+		$.cookie("darkTheme", "on");
+	}else{
+		$.cookie("darkTheme", "off");
+	}
+}
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
